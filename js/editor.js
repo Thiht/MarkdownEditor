9d50ed2c555ab06c1131941e7ffa118837be224d
Thibaut Rousseau <thiht44@gmail.com>
2013-05-27 10:08:35 +0200

    Minor update

--- js/editor.js
+++ js/editor.js
@@ -33,27 +33,73 @@ var MarkdownEditor = {
 		document.getElementById('pdf-overview').src = MarkdownEditor.doc.output('datauristring');
 	},
 
-	insertTag: function(startTag, endTag, tagType) {
-		var scroll = this.input.scrollTop;
+	insertTag: function(tagType) {
+		var scroll   = this.input.scrollTop,
+		    startTag = null,
+		    endTag   = null;
 		this.input.focus();
 
 		if (window.ActiveXObject) {
 			var textRange = document.selection.createRange(),
 			    currentSelection = textRange.text;
+		} else {
+			var startSelection   = this.input.value.substring(0, this.input.selectionStart),
+			    currentSelection = this.input.value.substring(this.input.selectionStart, this.input.selectionEnd),
+			    endSelection     = this.input.value.substring(this.input.selectionEnd);
+		}
 
+		if (tagType) {
+			if (tagType == 'image') {
+				var image = true;
+				tagType = 'link';
+			}
+			switch (tagType) {
+				case 'bold':
+					startTag = endTag = '**';
+				break;
+				case 'italic':
+					startTag = endTag = '*';
+				break;
+				case 'link':
+					if (currentSelection) {
+						if (currentSelection.indexOf('http://') == 0
+							|| currentSelection.indexOf('https://') == 0
+							|| currentSelection.indexOf('ftp://') == 0
+							|| currentSelection.indexOf('www.') == 0)
+						{
+							var label = prompt('Label?') || '';
+							startTag  = '[' + label + '](';
+							endTag    = ')';
+						} else {
+							var URL  = prompt('URL?');
+							startTag = '[';
+							endTag   = '](' + URL + ')';
+						}
+					} else {
+						var URL   = prompt('URL?') || '';
+						var label = prompt('Label?') || '';
+						startTag  = '[';
+						endTag    = '](' + URL + ')';
+						currentSelection = label;
+					}
+				break;
+			}
+			if (image) {
+				startTag = '!' + startTag;
+			}
+		}
+
+		if (window.ActiveXObject) {
 			textRange.text = startTag + currentSelection + endTag;
 			textRange.moveStart("character", -endTag.length - currentSelection.length);
 			textRange.moveEnd("character", -endTag.length);
 			textRange.select();
 		} else {
-			var startSelection   = this.input.value.substring(0, this.input.selectionStart),
-			    currentSelection = this.input.value.substring(this.input.selectionStart, this.input.selectionEnd),
-			    endSelection     = this.input.value.substring(this.input.selectionEnd);
-
 			this.input.value = startSelection + startTag + currentSelection + endTag + endSelection;
 			this.input.focus();
 			this.input.setSelectionRange(startSelection.length + startTag.length, startSelection.length + startTag.length + currentSelection.length);
 		}
+
 		this.input.scrollTop = scroll;
 		this.render();
 	}
